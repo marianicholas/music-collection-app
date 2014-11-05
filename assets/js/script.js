fec032a58fe13533f1178b8a8426ec019fd3ed86
maria.nicholas@gmail.com <mnicholas@Marias-MacBook-Air-2.local>
2014-11-05 11:07:51 -0500

    Latest

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function hover(element) {
+    element.setAttribute('src', 'assets/images/item-list-icon-green.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'assets/images/item-list-icon.png');
+}
+
+function hover(element) {
+    element.setAttribute('src', 'assets/images/add-item-icon-green.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'assets/images/add-item-icon.png');
+}
+
+function hover(element) {
+    element.setAttribute('src', 'assets/images/search-icon-green.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'assets/images/search-icon.png');
+}
